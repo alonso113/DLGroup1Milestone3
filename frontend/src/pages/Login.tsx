5e34dc4e90c5c8d11281cf359942faa8e4297a8a
alonso113 <geesinkantonalonso@gmail.com>
2025-12-12 17:59:26 +0100

    small fix

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -68,7 +68,7 @@ export const Login: React.FC = () => {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                  placeholder="moderator@fire-news.com"
+                  placeholder="Enter your email address"
                 />
               </div>
             </div>
